1807d6208766edec49087164ff30eb4c8bf0ffa7
msakuta <masahiro.sakuta@gmail.com>
2016-02-02 07:34:00 -0500

    Check collision between blocks

--- RealTetris.js
+++ RealTetris.js
@@ -23,10 +23,40 @@ Block.prototype.getH = function(){
 	return this.b - this.t;
 };
 
+/// Test intersection between blocks
+Block.prototype.intersects = function(r){
+	return this.r < r.l && r.r < this.l && this.b < r.t && r.b < this.t;
+};
+
+/// Set left top position of this block without changing size
+Block.prototype.set = function(x,y){
+	this.r -= this.l - x;
+	this.l = x;
+	this.b -= this.t - y;
+	this.t = y;
+};
+
+/// Update position by tracing intersecting blocks downwards
+Block.prototype.slipDown = function(ig, dest){
+	var ret = block_list.length;
+	var min = dest;
+
+	for(var i = 0; i < block_list.length; i++){
+		if(i != ig && this.l < block_list[i].r && block_list[i].l < this.r && this.b <= block_list[i].t && block_list[i].t < min){
+			ret = i;
+			min = block_list[i].t;
+		}
+	}
+
+	min -= this.b - this.t;
+	this.set(this.l, min);
+
+	return ret;
+};
+
 Block.prototype.update = function(){
-	var newtop = Math.min(height - this.getH(), this.t + 2);
-	this.b += newtop - this.t;
-	this.t = newtop;
+	var newbottom = Math.min(height, this.b + 2);
+	this.slipDown(null, newbottom);
 	this.updateGraphics();
 }
 
